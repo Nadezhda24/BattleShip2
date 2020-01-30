33b200b56318ba7b278baee3edf2e39b40b8b330
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2020-01-30 11:21:47 +0300

    Game

--- app/src/main/java/com/example/battleship/Activity_Game.java
+++ app/src/main/java/com/example/battleship/Activity_Game.java
@@ -2,7 +2,9 @@ package com.example.battleship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Game extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class Activity_Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__game);
+    }
+
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.Back: {
+                Intent intent = new Intent(Activity_Game.this, Activity_Menu.class);
+                startActivity(intent);
+            }
+            break;
+            case R.id.imageView: {
+                //настройки звука
+            }
+            break;
+            case R.id.Auto: {
+                //Чат
+            }
+            break;
+        }
+
     }
 }
