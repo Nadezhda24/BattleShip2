b511c8861b404aeba836eb2c14f63d95bbd3b01f
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2020-01-31 14:42:12 +0300

    Help and Setting

--- app/src/main/java/com/example/battleship/Activity_Help.java
+++ app/src/main/java/com/example/battleship/Activity_Help.java
@@ -2,7 +2,9 @@ package com.example.battleship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Help extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class Activity_Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.Back: {
+                Intent intent = new Intent(Activity_Help.this, Activity_Menu.class);
+                startActivity(intent);
+            }
+            break;
+            case R.id.imageView: {
+                //Настройка музыки
+            }
+            break;
+            case R.id.Left: {
+                //Прокрутка влево
+            }
+            break;
+            case R.id.Right: {
+                //Прокрутка вправо
+            }
+            break;
+        }
     }
 }
--- app/src/main/java/com/example/battleship/Activity_Setting.java
+++ app/src/main/java/com/example/battleship/Activity_Setting.java
@@ -2,7 +2,9 @@ package com.example.battleship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_Setting extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Activity_Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.Back: {
+                Intent intent = new Intent(Activity_Setting.this, Activity_Menu.class);
+                startActivity(intent);
+            }
+            break;
+            case R.id.Save: {
+                // сохранение изменений
+            }
+       }
     }
 }
